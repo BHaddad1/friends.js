168d3eca77015f59440688b5345909310c9b0a62
Blanche Haddad <blancherachel@gmail.com>
2022-09-21 15:25:39 -0600

    Add places array

---
+++ friends.js
@@ -0,0 +1,9 @@
+//var arrays = ["Rae", "Faye", "Kaitlyn", "Blanche", "Kelly"]
+
+//console.log(arrays[3])
+//console.log(arrays[4])
+//console.log(arrays[0])
+
+var places = ["Egypt", "Greece", "Norway", "Fiji", "Kenya"];
+
+console.log(places.pop());
